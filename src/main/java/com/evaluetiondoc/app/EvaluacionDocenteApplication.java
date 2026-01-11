d05192ced690f925b4e0c004ef85579fc79b81b6
juan.ortiz <juan.ortizf@infotec.mx>
2026-01-11 09:17:54 -0600

    se agrega proyecto inicial de evaluador

---
+++ src/main/java/com/evaluetiondoc/app/EvaluacionDocenteApplication.java
@@ -0,0 +1,13 @@
+package com.evaluetiondoc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionDocenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionDocenteApplication.class, args);
+	}
+
+}
